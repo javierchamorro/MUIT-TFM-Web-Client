e49bde97cf0bba5898713dd2ae6254d287538d40
Javier Chamorro <javierchamorro611@gmail.com>
2020-02-18 20:35:44 +0100

    Finalizar obtencion de informacion del usuario

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import './../assets/scss/main.scss';
 
-
-import { GLOBAL_CONFIG } from '../config/config.js';
+import {GLOBAL_CONFIG} from '../config/config.js';
 import * as I18n from '../vendors/I18n.js';
 import * as SAMPLES from '../config/samples.js';
 
@@ -15,28 +14,31 @@ import Quiz from './Quiz.jsx';
 import UserInfo from './UserInfo.jsx';
 
 export class App extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
+    this.state = {
+      startUp: true};
     I18n.init();
   }
 
   onAnswerUserInformation(){
-
-
-
+    if(SAMPLES.quiz_example.UserName==="" || SAMPLES.quiz_example.Age===0 || SAMPLES.quiz_example.Location==="" || SAMPLES.quiz_example.Studies===""){
+      console.log("Falta Info");
+    }else{
+      this.setState({startUp: false});
+    }
   }
 
-  render() {
+  render(){
     let appHeader = (<Header user_profile={this.props.user_profile} tracking={this.props.tracking} config={GLOBAL_CONFIG} I18n={I18n} />);
     let appHeader2 = "";
     let appContent = "";
-    let startUp = true;
-    if (startUp) {
-      appContent = (<UserInfo I18n={I18n}/>);
+    if(this.state.startUp){
+      appContent = (<UserInfo I18n={I18n} quiz={SAMPLES.quiz_example} onCompleteInformation={this.onAnswerUserInformation.bind(this)}/>);
     } else {
-      if ((this.props.tracking.finished !== true) || (GLOBAL_CONFIG.finish_screen === false)) {
+      if((this.props.tracking.finished !== true) || (GLOBAL_CONFIG.finish_screen === false)){
         appHeader2 = (<Header2 user_profile={this.props.user_profile} tracking={this.props.tracking} config={GLOBAL_CONFIG} I18n={I18n} />);
-        if (this.props.wait_for_user_profile !== true) {
+        if(this.props.wait_for_user_profile !== true){
           appContent = (<Quiz dispatch={this.props.dispatch} user_profile={this.props.user_profile} tracking={this.props.tracking} quiz={SAMPLES.quiz_example} config={GLOBAL_CONFIG} I18n={I18n} />);
         }
       } else {
@@ -54,7 +56,7 @@ export class App extends React.Component {
 
 }
 
-function mapStateToProps(state) {
+function mapStateToProps(state){
   return state;
 }
 
--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -1,28 +1,55 @@
 import React from 'react';
 
 export default class UserInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            age:0,
+            studies:'',
+            location:''};
+          }
+
+    onChangeNameText(valor){
+        this.setState({name: valor.target.value});
+        this.props.quiz.UserName = valor.target.value;
+    } 
+    onChangeStudiesText(valor){
+        this.setState({studies: valor.target.value});
+        this.props.quiz.Studies = valor.target.value;
+    }  
+    onChangeAgeText(valor){
+        this.setState({age: valor.target.value});
+        this.props.quiz.Age = valor.target.value;
+    }  
+    onChangeLocationText(valor){
+        this.setState({location: valor.target.value});
+        this.props.quiz.Location = valor.target.value;
+    }  
 
     render() {
 
         return (
             <div className="user">
-                <div className="name">
+                <div className="info">
                     <div className="nameText">{this.props.I18n.getTrans("i.nameAndSurname")}</div>
-                    <input name="name" />
+                    <input type="text" name="name" value={this.state.name} onChange={this.onChangeNameText.bind(this)}/>
                 </div>
-                <div className="age">
+                <div className="info">
                     <div className="ageText">{this.props.I18n.getTrans("i.age")}</div>
-                    <input name="age" />
+                    <input type="number" name="age" value={this.state.age} onChange={this.onChangeAgeText.bind(this)}/>
                 </div>
-                <div className="studies">
+                <div className="info">
                     <div className="studiesText">{this.props.I18n.getTrans("i.studies")}</div>
-                    <input name="studies" />
+                    <input type="text" name="studies" value={this.state.studies} onChange={this.onChangeStudiesText.bind(this)}/>
                 </div>
-                <div className="location">
+                <div className="info">
                     <div className="locationText">{this.props.I18n.getTrans("i.location")}</div>
-                    <input name="location" />
+                    <input type="text" name="location" value={this.state.location} onChange={this.onChangeLocationText.bind(this)}/>
                 </div>
-                <button className="answerUserInfo">{this.props.I18n.getTrans("i.answerUserInfo")}</button>
+                <div className="info">
+                    <button className="answerUserInfo" onClick={this.props.onCompleteInformation.bind(this)}>{this.props.I18n.getTrans("i.answerUserInfo")}</button>
+                </div>
             </div>
         );
 
